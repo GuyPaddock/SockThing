5de25e899a37769ed6b58cf8105d09b9f51525cf
gpaddock <guy.paddock@redbottledesign.com>
2013-12-26 23:11:24 -0500

    Move RPC interface code into different package.

---
+++ src/main/java/com/github/fireduck64/sockthing/rpc/bitcoin/BitcoinRpcConnection.java
@@ -0,0 +1,33 @@
+package com.github.fireduck64.sockthing.rpc.bitcoin;
+
+import java.io.IOException;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.google.bitcoin.core.Address;
+import com.google.bitcoin.core.Block;
+
+public interface BitcoinRpcConnection
+{
+    public abstract double getDifficulty()
+    throws IOException, JSONException;
+
+    public abstract int getBlockCount()
+    throws IOException, JSONException;
+
+    public abstract JSONObject getCurrentBlockTemplate()
+    throws IOException, JSONException;
+
+    public abstract long getBlockConfirmationCount(String blockHash)
+    throws IOException, JSONException;
+
+    public abstract JSONObject getBlockInfo(String blockHash)
+    throws IOException, JSONException;
+
+    public abstract boolean submitBlock(Block block)
+    throws IOException, JSONException;
+
+    public abstract String sendPayment(double amount, Address payFromAddress, Address payToAddress)
+    throws IOException, JSONException;
+}
