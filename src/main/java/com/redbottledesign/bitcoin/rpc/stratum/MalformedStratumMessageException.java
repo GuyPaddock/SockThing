09cbfa14f2522f345b68048f782bb90ef07bf331
gpaddock <gpaddock@RBD-Laptop-PC-03>
2014-01-22 10:27:52 -0500

    Javadoc clean-up.

--- src/main/java/com/redbottledesign/bitcoin/rpc/stratum/MalformedStratumMessageException.java
+++ src/main/java/com/redbottledesign/bitcoin/rpc/stratum/MalformedStratumMessageException.java
@@ -3,6 +3,15 @@ package com.redbottledesign.bitcoin.rpc.stratum;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
+/**
+ * <p>Exception thrown when JSON-encoded Stratum messages are either not
+ * recognized, malformed, or otherwise invalid according to a specific Stratum
+ * protocol specification.</p>
+ *
+ * <p>© 2013 - 2014 RedBottle Design, LLC.</p>
+ *
+ * @author Guy Paddock (guy.paddock@redbottledesign.com)
+ */
 public class MalformedStratumMessageException
 extends Exception
 {
@@ -11,30 +20,76 @@ extends Exception
      */
     private static final long serialVersionUID = -5169028037089628215L;
 
+    /**
+     * Initializes a new exception that indicates information in the specified
+     * JSON message is malformed.
+     *
+     * @param   jsonMessage
+     *          The JSON message that is malformed.
+     */
     public MalformedStratumMessageException(JSONObject jsonMessage)
     {
         this(jsonMessage.toString());
     }
 
+    /**
+     * Initializes a new exception that indicates information in the specified
+     * JSON message is malformed.
+     *
+     * @param   jsonMessage
+     *          The JSON message that is malformed, as a JSON {@code String}.
+     */
     public MalformedStratumMessageException(String jsonMessage)
     {
         super("Unknown or malformed Stratum JSON message received: " + jsonMessage);
     }
 
-    public MalformedStratumMessageException(JSONObject message, String error)
+    /**
+     * Initializes a new exception that indicates that information in the
+     * specified JSON message is malformed in the specified way.
+     *
+     * @param   jsonMessage
+     *          The JSON message that is malformed.
+     *
+     * @param   error
+     *          A description of the problem with the message.
+     */
+    public MalformedStratumMessageException(JSONObject jsonMessage, String error)
     {
         super(
             String.format(
                 "Unknown or malformed Stratum JSON message received (%s): %s",
                 error,
-                message.toString()));
+                jsonMessage.toString()));
     }
 
+    /**
+     * Initializes a new exception that indicates that information in the
+     * specified JSON message is malformed in a way that caused the specified
+     * exception.
+     *
+     * @param   jsonMessage
+     *          The JSON message that is malformed.
+     *
+     * @param   t
+     *          The exception that occurred while processing the message.
+     */
     public MalformedStratumMessageException(JSONObject jsonMessage, Throwable t)
     {
         this(jsonMessage.toString(), t);
     }
 
+    /**
+     * Initializes a new exception that indicates that information in the
+     * specified JSON message is malformed in a way that caused the specified
+     * exception.
+     *
+     * @param   jsonMessage
+     *          The JSON message that is malformed, as a JSON {@code String}.
+     *
+     * @param   t
+     *          The exception that occurred while processing the message.
+     */
     public MalformedStratumMessageException(String jsonMessage, Throwable t)
     {
         super(
@@ -44,87 +99,200 @@ extends Exception
                 jsonMessage),
             t);
     }
-    public MalformedStratumMessageException(String method, JSONObject message)
+
+    /**
+     * Initializes a new exception that indicates that the specified Stratum
+     * method was expected but not properly interpreted from the specified
+     * message.
+     *
+     * @param   method
+     *          The name of the method that was being interpreted.
+     *
+     * @param   jsonMessage
+     *          The JSON message that is malformed.
+     */
+    public MalformedStratumMessageException(String method, JSONObject jsonMessage)
     {
         super(
             String.format(
                 "Unknown or malformed \"%s\" Stratum JSON message received: %s",
                 method,
-                message.toString()));
+                jsonMessage.toString()));
     }
 
-    public MalformedStratumMessageException(String method, String error, JSONObject message)
+    /**
+     * Initializes a new exception that indicates that the specified Stratum
+     * method was expected but not properly interpreted from the specified
+     * message.
+     *
+     * @param   method
+     *          The name of the method that was being interpreted.
+     *
+     * @param   error
+     *          A description of the problem with the message.
+     *
+     * @param   jsonMessage
+     *          The JSON message that is malformed.
+     */
+    public MalformedStratumMessageException(String method, String error, JSONObject jsonMessage)
     {
         super(
             String.format(
                 "Unknown or malformed \"%s\" Stratum JSON message received (%s): %s",
                 method,
                 error,
-                message.toString()));
+                jsonMessage.toString()));
     }
 
-    public MalformedStratumMessageException(String method, String error, Throwable t, JSONObject message)
+    /**
+     * Initializes a new exception that indicates that the specified Stratum
+     * method was expected but not properly interpreted from the specified
+     * message, leading to the specified exception.
+     *
+     * @param   method
+     *          The name of the method that was being interpreted.
+     *
+     * @param   error
+     *          A description of the problem with the message.
+     *
+     * @param   t
+     *          The exception that occurred while processing the message.
+     *
+     * @param   jsonMessage
+     *          The JSON message that is malformed.
+     */
+    public MalformedStratumMessageException(String method, String error, Throwable t, JSONObject jsonMessage)
     {
         super(
             String.format(
                 "Unknown or malformed \"%s\" Stratum JSON message received (%s): %s",
                 method,
                 error,
-                message.toString()),
+                jsonMessage.toString()),
             t);
     }
 
-    public MalformedStratumMessageException(JSONArray message)
+    /**
+     * Initializes a new exception that indicates information in the specified
+     * JSON message is malformed.
+     *
+     * @param   jsonMessage
+     *          The JSON message that is malformed.
+     */
+    public MalformedStratumMessageException(JSONArray jsonMessage)
     {
-        super("Unknown or malformed Stratum JSON message received: " + message.toString());
+        this(jsonMessage.toString());
     }
 
-    public MalformedStratumMessageException(JSONArray message, String error)
+    /**
+     * Initializes a new exception that indicates that information in the
+     * specified JSON message is malformed in the specified way.
+     *
+     * @param   jsonMessage
+     *          The JSON message that is malformed.
+     *
+     * @param   error
+     *          A description of the problem with the message.
+     */
+    public MalformedStratumMessageException(JSONArray jsonMessage, String error)
     {
         super(
             String.format(
                 "Unknown or malformed Stratum JSON message received (%s): %s",
                 error,
-                message.toString()));
+                jsonMessage.toString()));
     }
 
-    public MalformedStratumMessageException(JSONArray message, Throwable t)
+    /**
+     * Initializes a new exception that indicates that information in the
+     * specified JSON message is malformed in a way that caused the specified
+     * exception.
+     *
+     * @param   jsonMessage
+     *          The JSON message that is malformed.
+     *
+     * @param   t
+     *          The exception that occurred while processing the message.
+     */
+    public MalformedStratumMessageException(JSONArray jsonMessage, Throwable t)
     {
         super(
             String.format(
                 "Unknown or malformed Stratum JSON message received (%s): %s",
                 t.getMessage(),
-                message.toString()),
+                jsonMessage.toString()),
             t);
     }
 
-    public MalformedStratumMessageException(String method, JSONArray message)
+    /**
+     * Initializes a new exception that indicates that the specified Stratum
+     * method was expected but not properly interpreted from the specified
+     * message.
+     *
+     * @param   method
+     *          The name of the method that was being interpreted.
+     *
+     * @param   jsonMessage
+     *          The JSON message that is malformed.
+     */
+    public MalformedStratumMessageException(String method, JSONArray jsonMessage)
     {
         super(
             String.format(
                 "Unknown or malformed \"%s\" Stratum JSON message received: %s",
                 method,
-                message.toString()));
+                jsonMessage.toString()));
     }
 
-    public MalformedStratumMessageException(String method, String error, JSONArray message)
+    /**
+     * Initializes a new exception that indicates that the specified Stratum
+     * method was expected but not properly interpreted from the specified
+     * message.
+     *
+     * @param   method
+     *          The name of the method that was being interpreted.
+     *
+     * @param   error
+     *          A description of the problem with the message.
+     *
+     * @param   jsonMessage
+     *          The JSON message that is malformed.
+     */
+    public MalformedStratumMessageException(String method, String error, JSONArray jsonMessage)
     {
         super(
             String.format(
                 "Unknown or malformed \"%s\" Stratum JSON message received (%s): %s",
                 method,
                 error,
-                message.toString()));
+                jsonMessage.toString()));
     }
 
-    public MalformedStratumMessageException(String method, String error, Throwable t, JSONArray message)
+    /**
+     * Initializes a new exception that indicates that the specified Stratum
+     * method was expected but not properly interpreted from the specified
+     * message, leading to the specified exception.
+     *
+     * @param   method
+     *          The name of the method that was being interpreted.
+     *
+     * @param   error
+     *          A description of the problem with the message.
+     *
+     * @param   t
+     *          The exception that occurred while processing the message.
+     *
+     * @param   jsonMessage
+     *          The JSON message that is malformed.
+     */
+    public MalformedStratumMessageException(String method, String error, Throwable t, JSONArray jsonMessage)
     {
         super(
             String.format(
                 "Unknown or malformed \"%s\" Stratum JSON message received (%s): %s",
                 method,
                 error,
-                message.toString()),
+                jsonMessage.toString()),
             t);
     }
 }
--- src/main/java/com/redbottledesign/bitcoin/rpc/stratum/message/PollRequestMessage.java
+++ src/main/java/com/redbottledesign/bitcoin/rpc/stratum/message/PollRequestMessage.java
@@ -2,28 +2,41 @@ package com.redbottledesign.bitcoin.rpc.stratum.message;
 
 import org.json.JSONObject;
 
-import com.redbottledesign.bitcoin.rpc.stratum.MalformedStratumMessageException;
-
+/**
+ * <p>Special type of request message used by non-TCP transports to poll the
+ * remote end for pending requests or responses.</p>
+ *
+ * <p>© 2013 - 2014 RedBottle Design, LLC.</p>
+ *
+ * @author Guy Paddock (guy.paddock@redbottledesign.com)
+ */
 public class PollRequestMessage
 extends RequestMessage
 {
-    public PollRequestMessage(JSONObject jsonMessage)
-    throws MalformedStratumMessageException
-    {
-        super(jsonMessage);
-    }
-
+    /**
+     * Default constructor for {@link PollRequestMessage}.
+     */
     public PollRequestMessage()
     {
         super(null, null);
     }
 
+    /**
+     * {@inheritDoc}
+     *
+     * <p>This override does nothing, and serves only to defeat the exception
+     * that is otherwise produced by having a {@code null} method name in the
+     * request message.</p>
+     */
     @Override
     protected void setMethodName(String methodName)
     {
         // Do nothing; defeat exception in base class
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public JSONObject toJson()
     {
